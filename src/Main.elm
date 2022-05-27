b0176fbe4ddf64bb6bfd5f72dc8268fa66a93612
Gaelan D'costa <gdcosta@gmail.com>
2022-05-27 19:29:53 -0400

    Can handle quadrant and sector changes

--- src/Main.elm
+++ src/Main.elm
@@ -2,31 +2,41 @@ module Main exposing (travel)
 
 
 type alias Location =
-    ( Int, Int )
+    { qx : Int, qy : Int, sx : Int, sy : Int }
 
 
 travel : Float -> Float -> Location -> Location
-travel course warp_factor ( old_x, old_y ) =
+travel course warp_factor { qx, qy, sx, sy } =
     let
-        -- Map course direction to an angle
+        -- Map course direction to an radian
         angle =
             -(pi * (course - 1.0) / 4.0)
 
-        -- Get 1d direction vectors via that angle
-        direction_x =
-            warp_factor * cos angle
+        -- 1.000 warp factor moves you 1 quadrant exactly
+        -- (i.e. 8 sectors' worth of distance)
+        quadrant_magnitude = floor warp_factor |> toFloat
+        -- 0.125 warp factor moves you one sector position
+        sector_magnitude = (warp_factor - quadrant_magnitude) * 8
 
-        direction_y =
-            warp_factor * sin angle
+        -- Get one-dimensional direction unit vectors via the angle
+        v_x = cos angle
+        v_y = sin angle
 
-        -- 0.125 warp factor moves you one sector
-        magnitude_x =
-            direction_x / 0.125
+        -- How many quadrants should we move (i.e. translate)?
+        t_qx =
+            quadrant_magnitude * v_x |> round
+        t_qy =
+            quadrant_magnitude * v_y |> round
 
-        magnitude_y =
-            direction_y / 0.125
+        -- How many sectors should we move (i.e. translate)?
+        t_sx =
+            sector_magnitude * v_x |> round
+        t_sy =
+            sector_magnitude * v_y |> round
     in
-    ( -- Approximate calculations to our integer-based positioning system
-      old_x + round magnitude_x
-    , old_y + round magnitude_y
-    )
+    {
+      qx = qx + t_qx
+    , qy = qy + t_qy
+    , sx = sx + t_sx
+    , sy = sy + t_sy
+    }
--- tests/MainTests.elm
+++ tests/MainTests.elm
@@ -1,16 +1,32 @@
 module MainTests exposing (..)
 
-import Expect exposing (Expectation)
+import Expect
 import Main
-import Test exposing (..)
+import Test exposing (describe, test)
 
 
 suite =
-    test "that in-sector example from the youtube video"
-        (\_ ->
-            let
-                oldLoc =
-                    ( 5, 3 )
-            in
-            Main.travel 8 0.125 oldLoc |> Expect.equal ( 6, 4 )
-        )
+    describe "Navigation tests"
+        [ test "that in-sector example from the youtube video"
+            (\_ ->
+                let
+                    oldLoc =
+                        { qx = 0, qy = 0, sx = 5, sy = 3 }
+
+                    newLoc =
+                        { qx = 0, qy = 0, sx = 6, sy = 4 }
+                in
+                Main.travel 8 0.125 oldLoc |> Expect.equal newLoc
+            )
+        , test "can we warp exactly one quadrant away?"
+            (\_ ->
+                let
+                    oldLoc =
+                        { qx = 0, qy = 0, sx = 5, sy = 3 }
+
+                    newLoc =
+                        { qx = 1, qy = 1, sx = 5, sy = 3 }
+                in
+                Main.travel 8 1 oldLoc |> Expect.equal newLoc
+            )
+        ]
